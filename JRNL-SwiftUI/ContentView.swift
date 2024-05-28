9ca514cd02f495cce1f4524afe44673ff73f822f
Jin Park <jinpark1504@gmail.com>
2024-05-28 17:02:53 +0900

    Initial Commit

---
+++ JRNL-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JRNL-SwiftUI
+//
+//  Created by Jungjin Park on 2024-05-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JRNL-SwiftUI/JRNL_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRNL_SwiftUIApp.swift
+//  JRNL-SwiftUI
+//
+//  Created by Jungjin Park on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct JRNL_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
